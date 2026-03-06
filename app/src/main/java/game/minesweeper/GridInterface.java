b1369bfd4ad9e2aa8f29e85340b63217e1ab6743
Leo <gioleo999@gmail.com>
2026-03-06 10:49:22 +0100

    fix: remove redundant key word

--- app/src/main/java/game/minesweeper/GridInterface.java
+++ app/src/main/java/game/minesweeper/GridInterface.java
@@ -5,13 +5,11 @@ import java.util.List;
 
 public interface GridInterface<C extends CoordinateInterface> {
 
-    void initCells();
-
     Cell getCell(C coordinate);
 
     List<Cell> getCellNeighbors(C coordinate);
 
     List<Cell> getAllCells();
 
-    public List<C> getAllCoordinates();
+    List<C> getAllCoordinates();
 }
