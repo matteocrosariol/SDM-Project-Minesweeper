512d75f0b1fbd8983e8a9d100709a205238cc0ef
matteocrosariol <matteo.crosariol@gmail.com>
2026-03-24 22:03:08 +0100

    usefull test removed in the minesweeper test class

--- app/src/test/java/game/minesweeper/MinesweeperTest.java
+++ app/src/test/java/game/minesweeper/MinesweeperTest.java
@@ -4,9 +4,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MinesweeperTest {
-    @Test
-    void appHasAGreeting() {
-        Minesweeper classUnderTest = new Minesweeper();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+
 }
